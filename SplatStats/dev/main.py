1bf74f680a5fc02d8164b689a02c623676cc8467
chipdelmal <chipdelmal@gmail.com>
2022-09-19 20:25:17 -0700

    Kill ratio simple plot

--- SplatStats/dev/main.py
+++ SplatStats/dev/main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import SplatStats as splat
+import matplotlib.pyplot as plt
 
 (iPath, oPath) = (
     '/home/chipdelmal/Documents/GitHub/s3s/',
@@ -17,4 +18,16 @@ history = splat.History(iPath, oPath)
 # Get the battles filepaths
 playerHistory = history.getPlayerHistory('čħîþ ウナギ')
 validMatches = playerHistory[playerHistory['win']!='NA']
-validMatches
+
+(k, d) = (list(validMatches['kill']), list(validMatches['death']))
+
+(fig, ax) = plt.subplots(figsize=(20, 8))
+ax.plot(k, 'o', color='blue', alpha=0.3)
+ax.plot(d, 'o', color='red', alpha=0.3)
+for i in range(len(k)):
+    color = 'blue' if k[i] > d[i] else 'red'
+    ax.vlines(i, k[i], d[i], color=color, alpha=0.2)
+# ax.hlines([0], 0, 1, color='k', transform=ax.get_yaxis_transform())
+ax.set_aspect(.5/ax.get_data_ratio())
+ax.set_xlim(-1, len(k))
+ax.set_ylim(-1, max(k)+1)
