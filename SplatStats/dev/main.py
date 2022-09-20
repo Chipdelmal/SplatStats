4bb38f812643b3373c265d14a4e5134410049371
chipdelmal <chipdelmal@gmail.com>
2022-09-19 20:36:14 -0700

    Minor improvement to k/d plot

--- SplatStats/dev/main.py
+++ SplatStats/dev/main.py
@@ -18,16 +18,19 @@ history = splat.History(iPath, oPath)
 # Get the battles filepaths
 playerHistory = history.getPlayerHistory('čħîþ ウナギ')
 validMatches = playerHistory[playerHistory['win']!='NA']
-
 (k, d) = (list(validMatches['kill']), list(validMatches['death']))
+t = list(validMatches['match type'])
 
+# Use match type for point shape
 (fig, ax) = plt.subplots(figsize=(20, 8))
-ax.plot(k, 'o', color='blue', alpha=0.3)
-ax.plot(d, 'o', color='red', alpha=0.3)
 for i in range(len(k)):
-    color = 'blue' if k[i] > d[i] else 'red'
+    shape = 'X' if t[i] != 'Turf War' else 'o'
+    color = 'blue' if k[i] >= d[i] else 'red'
+    ax.plot(i, k[i], shape, color=color, alpha=0.3)
+    ax.plot(i, d[i], shape, color=color, alpha=0.3)
     ax.vlines(i, k[i], d[i], color=color, alpha=0.2)
 # ax.hlines([0], 0, 1, color='k', transform=ax.get_yaxis_transform())
 ax.set_aspect(.5/ax.get_data_ratio())
 ax.set_xlim(-1, len(k))
 ax.set_ylim(-1, max(k)+1)
+
