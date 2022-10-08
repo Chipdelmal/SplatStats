d0eb18a990483e7c9fbce4efd131150981ec8a07
chipdelmal <chipdelmal@gmail.com>
2022-10-07 21:29:45 -0700

    Iris plot getting closer

--- SplatStats/dev/plotDev.py
+++ SplatStats/dev/plotDev.py
@@ -9,7 +9,7 @@ import SplatStats as splat
 from pywaffle import Waffle
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
-
+# plt.rcParams["font.family"]="Roboto Mono"
 
 if splat.isNotebook():
     (iPath, oPath) = (
@@ -28,23 +28,23 @@ bPaths = splat.getBattleFilepaths(oPath)
 # Create Player Objects
 ###############################################################################
 NAMES = (
-    'čħîþ ウナギ', 'Yami ウナギ', 'Riché ウナギ',
-    'Oswal　ウナギ', 'April ウナギ', 'Murazee'
+    'čħîþ ウナギ', 'Yami ウナギ', 'Riché ウナギ', 'DantoNnoob',
+    'Oswal　ウナギ', 'April ウナギ', 'Murazee', 'Rei ウナギ'
 )
-plyr = splat.Player(NAMES[3], bPaths, timezone='America/Los_Angeles')
+plyr = splat.Player(NAMES[0], bPaths, timezone='America/Los_Angeles')
 playerHistory = plyr.battlesHistory
 ###############################################################################
-#  Waffle Dev
+# Iris Dev 
 ###############################################################################
 bHist = playerHistory
-PLUS = 10
+PLUS = .75
 nEntry = bHist.shape[0]
-(RMIN, RMAX) = (0, 30)
+(RMIN, RMAX) = (0, 40)
 (PMIN, PMAX) = (0, 2000)
 (ANGLES, BOTTOM, TOP, PAINT) = (
-    np.linspace(0, 2*np.pi, bHist.shape[0], endpoint=False),
+    np.linspace(2*np.pi, 0, bHist.shape[0], endpoint=False),
     np.array(bHist['death']),
-    np.array(bHist['kill']),
+    np.array(bHist['kill'])+.5*np.array(bHist['assist']),
     np.array(bHist['paint'])
 )
 PAINT = np.array([np.interp(i, (PMIN, PMAX), (RMIN, RMAX)) for i in PAINT])
@@ -55,20 +55,79 @@ CLRS = [
 ]
 (fig, ax) = plt.subplots(figsize=(8, 8), subplot_kw={"projection": "polar"})
 ax.set_theta_offset(np.pi/2)
+ax.set_rscale('symlog')
 ax.vlines(ANGLES, PLUS, PLUS+PAINT,  lw=1, colors=splat.CLR_PAINT, alpha=.1)
 ax.vlines(ANGLES, PLUS+BOTTOM, PLUS+TOP, lw=0.5, colors=CLRS, alpha=.7)
-ax.set_yticks(np.arange(RMIN+PLUS, RMAX+PLUS, 10))
+ax.set_yticks(np.arange(RMIN+PLUS, RMAX+PLUS, 5))
 ax.set_yticklabels([])
 xlab = [bHist['datetime'].iloc[int(dt)] for dt in np.arange(0,nEntry, nEntry/10)]
 ax.set_xticks([])
-# ax.set_xticks(np.arange(0, 2*np.pi, 2*np.pi/10), lw=0)
-# ax.set_xticklabels([f"{i.day:02d}/{i.month:02d}" for i in xlab])
+ax.spines["start"].set_color("none")
+ax.spines["polar"].set_color("#00000088")
+ax.yaxis.grid(True, color='#00000011', ls='--', lw=0.25)
 ax.set_ylim(RMIN, RMAX+PLUS)
+for pos in ['bottom']:
+    print(plt.gca().spines)#~[pos].set_visible(False)
+ax.text(
+    x=0.5, y=0.5, 
+    s=f"{plyr.playerStats['general']['kassists ratio']:.2f}",
+    va="center", ha="center",  ma="center", color='#00000066',
+    fontsize=20, linespacing=0.87, transform=ax.transAxes
+)
 fig.savefig(
     path.join(oPath, (plyr.name)+' Iris.png'), 
     dpi=200, bbox_inches='tight', facecolor=fig.get_facecolor()
 )
 
+
+
+def plotIris(
+        figAx, 
+        topArray, bottomArray=None, barArray=None, 
+        kRange=(0, 50), pRange=(0, 2000),
+        rScale='symlog', innerOffset=0.75, clockwise=True,
+        colorsTop=(splat.CLR_STATS['kill'], splat.CLR_STATS['death']),
+        colorBars=splat.CLR_PAINT
+    ):
+    (fig, ax) = figAx
+    ax.set_theta_offset(np.pi/2)
+    ax.set_rscale(rScale)
+    # Calculate angles for marker lines ---------------------------------------
+    DLEN = topArray.shape[0]
+    (astart, aend) = ((2*np.pi, 0) if True else (0, 2*np.pi))
+    ANGLES = np.linspace(astart, aend, DLEN, endpoint=False)
+    # Draw top-bottom ---------------------------------------------------------
+    if bottomArray is None:
+        bottomArray = np.zeros(topArray.shape)
+    heights = topArray-bottomArray
+    colors = [colorsTop[0] if (h>=0) else colorsTop[1] for h in heights]
+    ax.vlines(
+        ANGLES, innerOffset+bottomArray, innerOffset+topArray, 
+        lw=0.5, colors=colors, alpha=.7
+    )
+    # Draw bar ----------------------------------------------------------------
+    if barArray is None:
+        barArray = np.zeros(topArray.shape)
+    ax.vlines(
+        ANGLES, innerOffset, innerOffset+barArray,  
+        lw=1, colors=colorBars, alpha=.1
+    )
+    # Cleanin up axes ---------------------------------------------------------
+    ax.set_xticks([])
+    ax.set_ylim(kRange[0], kRange[1]+innerOffset)
+    ax.set_yticklabels([])
+    ax.set_yticks(np.arange(kRange[0]+innerOffset, kRange[1]+innerOffset, 5))
+    ax.yaxis.grid(True, color='#00000011', ls='--', lw=0.25)
+    ax.spines["start"].set_color("none")
+    ax.spines["polar"].set_color("#00000088")
+    # Return figure -----------------------------------------------------------
+    return (fig, ax)
+    
+(fig, ax) = plt.subplots(figsize=(8, 8), subplot_kw={"projection": "polar"})
+plotIris(
+    (fig, ax), TOP, BOTTOM, PAINT,
+    kRange=(0, 40)
+)
 
 ###############################################################################
 # Windowed average
