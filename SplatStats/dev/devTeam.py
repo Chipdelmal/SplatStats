e9c0dcb19c95e597cbdcb215573ace81ce5454af
chipdelmal <chipdelmal@gmail.com>
2022-11-12 13:04:45 -0800

    Getting team streamchart together

--- SplatStats/dev/devTeam.py
+++ SplatStats/dev/devTeam.py
@@ -16,7 +16,7 @@ from SplatStats.constants import MKR_STATS
 if splat.isNotebook():
     (iPath, oPath) = (
         path.expanduser('~/Documents/GitHub/s3s/'),
-        path.expanduser('~/Documents/Sync/BattlesData/')
+        path.expanduser('~/Sync/BattlesData/')
     )
 else:
     (iPath, oPath) = argv[1:]
@@ -31,11 +31,63 @@ NAMES = (
     'čħîþ ウナギ', 'Yami ウナギ', 'Riché ウナギ', 'Oswal　ウナギ',
     'April ウナギ', 'Rei ウナギ', 'DantoNnoob', 'Murazee'
 )
+COLORS = (
+    
+)
 TZ = 'America/Los_Angeles'
 team = splat.Team(NAMES, bPaths, TZ)
 teamHist = team.battleHistory
 teamHistBT = team.reshapeTeamHistoryByPeriod(
     period='24H'
 )
-teamHistBT.iloc[-100:-50]
+###############################################################################
+# Plotting Stream
+###############################################################################
+(fig, ax) = plt.subplots(figsize=(10, 2))
+metric = 'kill'
+normalized = False
+smooth = True
+smoothness = 0.75
+gridRes = 500
+baseline = 'sym'
+colors = [
+    "#0D40DE", "#EC0B68", "#6ABF0B", "#9090BA",
+    "#A577FF", "#941A88", "#CFD1C7", "#E4E567" 
+]
+# Reshape dataframe and get vars ----------------------------------------------
 dfByPlayer = teamHistBT.reorder_levels(["player", "datetime"])
+names = team.names
+entriesNum = dfByPlayer.loc[names[0]].shape[0]
+# Populate stream -------------------------------------------------------------
+stream = np.zeros((len(names), entriesNum))
+for (ix, name) in enumerate(names):
+    stream[ix]  = np.array(dfByPlayer.loc[name][metric])
+streamFiltered = stream[:,np.any(stream>0, axis=0)]
+# Normalize if needed ---------------------------------------------------------
+if normalized:
+    cSum = np.sum(streamFiltered, axis=0)
+    streamFiltered = np.array([r/cSum for r in streamFiltered])
+# Plot variables --------------------------------------------------------------
+x = range(streamFiltered.shape[1])
+if smooth:
+    grid = np.linspace(0, streamFiltered.shape[1], num=gridRes)
+    y = [splat.gaussian_smooth(x, i, grid, smoothness) for i in streamFiltered]
+else:
+    y = streamFiltered
+# Generate figure -------------------------------------------------------------
+ax.stackplot(grid, y, baseline="sym", colors=COLORS, alpha=.9)
+ax.set_xlim(0, max(x))
+ax.set_xticks([])
+ax.set_yticks([])
+# ax.axis('off')
+ax.legend(
+    names, loc='upper left', frameon=False,
+    bbox_to_anchor=(1, 1), ncol=2
+)
+
+
+
+fig.savefig(
+    path.join(oPath, f'Wave.png'), 
+    dpi=300, bbox_inches='tight', facecolor=fig.get_facecolor()
+)
