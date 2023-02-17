95fa8b7edd5c86f326bc4f0e8450095f737fdc77
chipdelmal <chipdelmal@gmail.com>
2023-02-17 14:36:51 -0800

    Pre-merge from lab

--- SplatStats/constantsStatInk.py
+++ SplatStats/constantsStatInk.py
@@ -1,24 +1,81 @@
 
 import json
 
-with open('./InkStat/weapon.json', 'r') as f:
-  WEAPONS = json.load(f)
-wpnKeys = [i['key'] for i in WEAPONS]
+# with open('./InkStat/weapon.json', 'r') as f:
+#   WEAPONS = json.load(f)
+# wpnKeys = [i['key'] for i in WEAPONS]
 
-
-GAME_MODE = {
-    'nawabari': 'Turf War',
-    'area': 'Splat Zones',
-    'yagura': 'Tower Control',
-    'hoko': 'Rainmaker',
-    'asari': 'Clam Blitz'
-}
 
 LOBBY_MODE = {
-    'regular': 'Regular',
-    'bankara_challenge': 'Anarchy (Series)',
-    'bankara_open': 'Anarchy (Open)',
-    'xmatch': 'X Battle',
-    'splatfest_challenge': 'Splatfest (Pro)',
-    'splatfest_open': 'Splatfest (Open)'
+  'regular': 'Regular',
+  'bankara_challenge': 'Anarchy (Series)',
+  'bankara_open': 'Anarchy (Open)',
+  'xmatch': 'X Battle',
+  'splatfest_challenge': 'Splatfest (Pro)',
+  'splatfest_open': 'Splatfest (Open)'
+}
+GAME_MODE = {
+  'nawabari': 'Turf War',
+  'area': 'Splat Zones',
+  'yagura': 'Tower Control',
+  'hoko': 'Rainmaker',
+  'asari': 'Clam Blitz'
+}
+STATINK_DTYPES = {
+  # Match information -------------------------------------------------------
+  '# season': 'string',
+  #'period': 'datetime64'
+  'game-ver': 'string',
+  'lobby': 'string',
+  'mode': 'string',
+  'stage': 'string',
+  'time': 'uint16',
+  'win': 'string',
+  'knockout': 'string',
+  'rank': 'string',
+  'power': 'float',
+  # Team results ------------------------------------------------------------
+  'alpha-inked': 'Int64',         'bravo-inked': 'Int64',
+  'alpha-ink-percent': 'float',   'bravo-ink-percent': 'float',
+  'alpha-count': 'Int64',         'bravo-count': 'Int64',
+  'alpha-color': 'string',        'bravo-color': 'string',
+  'alpha-theme': 'string',        'bravo-theme': 'string',
+  # Team A stats ------------------------------------------------------------
+  'A1-weapon': 'string',   'A1-kill-assist': 'Int8',
+  'A1-kill': 'Int8',       'A1-assist': 'Int8', 
+  'A1-death': 'Int8',      'A1-special': 'Int8',
+  'A1-inked': 'Int16',     'A1-abilities': 'object',
+  'A2-weapon': 'string',   'A2-kill-assist': 'Int8',
+  'A2-kill': 'Int8',       'A2-assist': 'Int8', 
+  'A2-death': 'Int8',      'A2-special': 'Int8',
+  'A2-inked': 'Int16',     'A2-abilities': 'object',
+  'A3-weapon': 'string',   'A3-kill-assist': 'Int8',
+  'A3-kill': 'Int8',       'A3-assist': 'Int8', 
+  'A3-death': 'Int8',      'A3-special': 'Int8',
+  'A3-inked': 'Int16',     'A3-abilities': 'object',
+  'A4-weapon': 'string',   'A4-kill-assist': 'Int8',
+  'A4-kill': 'Int8',       'A4-assist': 'Int8', 
+  'A4-death': 'Int8',      'A4-special': 'Int8',
+  'A4-inked': 'Int16',     'A4-abilities': 'object',
+  # Team B stats ------------------------------------------------------------
+  'B1-weapon': 'string',   'B1-kill-assist': 'Int8',
+  'B1-kill': 'Int8',       'B1-assist': 'Int8', 
+  'B1-death': 'Int8',      'B1-special': 'Int8',
+  'B1-inked': 'Int16',     'B1-abilities': 'object',
+  'B2-weapon': 'string',   'B2-kill-assist': 'Int8',
+  'B2-kill': 'Int8',       'B2-assist': 'Int8', 
+  'B2-death': 'Int8',      'B2-special': 'Int8',
+  'B2-inked': 'Int16',     'B2-abilities': 'object',
+  'B3-weapon': 'string',   'B3-kill-assist': 'Int8',
+  'B3-kill': 'Int8',       'B3-assist': 'Int8', 
+  'B3-death': 'Int8',      'B3-special': 'Int8',
+  'B3-inked': 'Int16',     'B3-abilities': 'object',
+  'B4-weapon': 'string',   'B4-kill-assist': 'Int8',
+  'B4-kill': 'Int8',       'B4-assist': 'Int8', 
+  'B4-death': 'Int8',      'B4-special': 'Int8',
+  'B4-inked': 'Int16',     'A4-abilities': 'object',
+  # Medals ------------------------------------------------------------------
+  'medal1-name': 'string', 'medal1-grade': 'string',
+  'medal2-name': 'string', 'medal2-grade': 'string',
+  'medal3-name': 'string', 'medal3-grade': 'string'    
 }
--- SplatStats/dev/statInk.py
+++ SplatStats/dev/statInk.py
@@ -2,44 +2,20 @@
 # https://github.com/fetus-hina/stat.ink/wiki/Spl3-%EF%BC%8D-CSV-Schema-%EF%BC%8D-Battle
 # https://stat.ink/api-info/weapon3
 
+import re
+import pandas as pd
 from os import path
 from glob import glob
-import pandas as pd
+import matplotlib.pyplot as plt
+from collections import Counter
+import SplatStats as splat
 
-DATA_PATH = '/Users/sanchez.hmsc/Downloads/InkStats'
+DATA_PATH = '/home/chipdelmal/Documents/Sync/BattlesDocker/battle-results-csv'
 FPATHS = glob(path.join(DATA_PATH, '*-*-*.csv'))
-
-
-LOBBY_MODE = {
-    'regular': 'Regular',
-    'bankara_challenge': 'Anarchy (Series)',
-    'bankara_open': 'Anarchy (Open)',
-    'xmatch': 'X Battle',
-    'splatfest_challenge': 'Splatfest (Pro)',
-    'splatfest_open': 'Splatfest (Open)'
-}
-GAME_MODE = {
-    'nawabari': 'Turf War',
-    'area': 'Splat Zones',
-    'yagura': 'Tower Control',
-    'hoko': 'Rainmaker',
-    'asari': 'Clam Blitz'
-}
-
-dTypes = {
-    '# season': 'string',
-    #'period': 'datetime64'
-    'game-ver': 'string',
-    'lobby': 'string',
-    'mode': 'string',
-    'stage': 'string',
-    'time': 'uint16',
-    'rank': 'string'
-}
 ###############################################################################
 # Read Full Data
 ###############################################################################
-DFS_LIST = [pd.read_csv(f, dtype=dTypes, parse_dates=['period']) for f in FPATHS]
+DFS_LIST = [pd.read_csv(f, dtype=splat.STATINK_DTYPES, parse_dates=['period']) for f in FPATHS]
 FULL_DF = pd.concat(DFS_LIST)
 cols = [i.replace('#', '').strip() for i in list(FULL_DF.columns)]
 df = FULL_DF.copy()
@@ -47,6 +23,22 @@ df = FULL_DF.copy()
 # Make Replacements
 ###############################################################################
 df.columns = cols
-df['lobby'] = [LOBBY_MODE[lob] for lob in df['lobby']]
-df['mode']  = [GAME_MODE[lob] for lob in df['mode']]
+df['lobby'] = [splat.LOBBY_MODE[lob] for lob in df['lobby']]
+df['mode']  = [splat.GAME_MODE[mod] for mod in df['mode']]
 
+
+r = re.compile(".*weapon*")
+wpnCols = list(filter(r.match, cols))
+
+
+
+
+[Counter(df[i]) for i in wpnCols]
+
+###############################################################################
+# Aggregate by date
+###############################################################################
+df['dummy'] = [1]*df.shape[0]
+counts = df.groupby([df['period'].dt.date]).count()['dummy']
+(fig, ax) = (plt.figure(), plt.axes())
+ax.plot(list(counts))
