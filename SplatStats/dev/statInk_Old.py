5742d8163daec745036fc80f9a2c2ed4252ad62a
chipdelmal <chipdelmal@gmail.com>
2023-04-04 11:20:31 -0700

    Weapon xray created for inkstat

--- SplatStats/dev/statInk_Old.py
+++ SplatStats/dev/statInk_Old.py
@@ -53,12 +53,47 @@ for prepend in ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4']:
     dfs.append(df)
 dfStats = pd.concat(dfs)
 dfStats['kassist'] = dfStats['kill']+dfStats['assist']/2
+dfStats['paint'] = dfStats['inked']/100
 weapons = sorted(list(dfStats['weapon'].unique()))
 
-wpn = weapons[63]
-print(wpn)
-xRan = (0, 20)
-binSize = 1
+
+STATS = {
+    'stat': ['kill', 'death', 'assist', 'special', 'paint'],
+    'colors': ['#1A1AAEDD', '#C12D74DD', '#35BA49DD', '#801AB3DD', '#EE8711DD']
+}
+STATS['cmaps'] = [
+    splat.colorPaletteFromHexList(['#ffffff00', c]) for c in STATS['colors']
+]
+
+
+(xRan, binSize) = ((0, 20), 1)
+wix = weapons.index('Splattershot')
+wpn = weapons[wix]
+fltrd = dfStats[dfStats['weapon']==wpn]
+fltrdStat = [fltrd[i] for i in STATS['stat']]
+(fig, ax) = plt.subplots(figsize=(20, 2))
+for (ix, st) in enumerate(fltrdStat):
+    kFreqs = splat.calcBinnedFrequencies(st, xRan[0], xRan[1], normalized=True)
+    for (x, k) in enumerate(kFreqs):
+        ax.add_patch(
+            Rectangle(
+                (x, ix), binSize, 1, 
+                facecolor=STATS['cmaps'][ix](k*10), edgecolor='#000000',
+            )
+        )
+ax.set_title(wpn)
+ax.set_xticklabels([])
+ax.set_yticklabels([])
+ax.set_xlim(*xRan)
+ax.set_ylim(0, len(STATS['stat']))
+
+
+
+
+
+
+
+
 
 fltrd = dfStats[dfStats['weapon']==wpn]
 fltrdStat = [fltrd[i] for i in ['kassist', 'death']]
