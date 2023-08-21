e665df3b398838c34147f9f3cb0e077e4ec5afc2
chipdelmal <chipdelmal@gmail.com>
2023-08-20 20:35:10 -0700

    Added frequency to KAD barchart

--- SplatStats/dev/dta_SAExploration.py
+++ SplatStats/dev/dta_SAExploration.py
@@ -36,77 +36,81 @@ NAMES = (
     'čħîþ ウナギ', 'Yami ウナギ', 'Riché ウナギ', 'Oswal　ウナギ',
     'April ウナギ', 'Rei ウナギ', 'DantoNnoob', 'Murazee', 'HSR'
 )
-# for plyrName in NAMES:
-###############################################################################
-# Auxiliary 
-###############################################################################
-fNameID = f'{plyrName}-{weapon}'
-splat.setSplatoonFont(fontPath, fontName="Splatfont 2")
-###############################################################################
-# Process JSON files into battle objects
-###############################################################################
-hFilepaths = splat.getDataFilepaths(iPath, filePat='results.json')
-bPaths = splat.dumpBattlesFromJSONS(hFilepaths, bPath, overwrite=overwrite)
-bFilepaths = splat.getBattleFilepaths(bPath)
-###############################################################################
-# Create Player Object
-###############################################################################
-plyr = splat.Player(plyrName, bFilepaths, timezone='America/Los_Angeles')
-playerHistory = plyr.battlesHistory
-playerHistory = playerHistory[playerHistory['match mode']!='PRIVATE']
-# Weapon filter ---------------------------------------------------------------
-if weapon!='All':
-    playerHistory = playerHistory[playerHistory['main weapon']==weapon]
-else:
-    playerHistory = playerHistory
-###############################################################################
-# Initial Explorations 
-###############################################################################
-stat='kad'
+for plyrName in NAMES:
+    ###############################################################################
+    # Auxiliary 
+    ###############################################################################
+    fNameID = f'{plyrName}-{weapon}'
+    splat.setSplatoonFont(fontPath, fontName="Splatfont 2")
+    ###############################################################################
+    # Process JSON files into battle objects
+    ###############################################################################
+    hFilepaths = splat.getDataFilepaths(iPath, filePat='results.json')
+    bPaths = splat.dumpBattlesFromJSONS(hFilepaths, bPath, overwrite=overwrite)
+    bFilepaths = splat.getBattleFilepaths(bPath)
+    ###############################################################################
+    # Create Player Object
+    ###############################################################################
+    plyr = splat.Player(plyrName, bFilepaths, timezone='America/Los_Angeles')
+    playerHistory = plyr.battlesHistory
+    playerHistory = playerHistory[playerHistory['match mode']!='PRIVATE']
+    # Weapon filter ---------------------------------------------------------------
+    if weapon!='All':
+        playerHistory = playerHistory[playerHistory['main weapon']==weapon]
+    else:
+        playerHistory = playerHistory
+    ###############################################################################
+    # Initial Explorations 
+    ###############################################################################
+    stat='kad'
 
-playerHistory['kad2'] = ((playerHistory['kill']+0.5*playerHistory['assist'])) / [max(i, 1) for i in playerHistory['death']]
+    playerHistory['kad2'] = ((playerHistory['kill']+0.5*playerHistory['assist'])) / [max(i, 1) for i in playerHistory['death']]
 
-results = []
-(delta, xlim) = (0.5, 25+0.5)
-totalMatches = playerHistory.shape[0]
-for threshold in np.arange(0, xlim+delta, delta):
-    (won, lost) = [playerHistory[playerHistory['winBool']==i] for i in (1, 0)]
-    (wonCount, lostCount) = (
-        won[won[stat]>=threshold].shape[0],
-        lost[lost[stat]>=threshold].shape[0]
+    results = []
+    (delta, xlim, cumFreq) = (0.5, 25+0.5, False)
+    totalMatches = playerHistory.shape[0]
+    for threshold in np.arange(0, xlim+delta, delta):
+        (won, lost) = [playerHistory[playerHistory['winBool']==i] for i in (1, 0)]
+        (wonCount, lostCount) = (
+            won[won[stat]>=threshold].shape[0],
+            lost[lost[stat]>=threshold].shape[0]
+        )
+        ratio = (wonCount+lostCount) and wonCount / (wonCount+lostCount) or 0
+        totRange = sum((playerHistory[stat]>=threshold)&(playerHistory[stat]<threshold+delta))
+        if cumFreq:
+            matchFreq = (wonCount+lostCount)/totalMatches
+        else:
+            matchFreq = totRange/totalMatches
+        results.append((threshold, ratio, matchFreq))
+    top = min([i[0] for i in results if i[1]==0])
+
+    (fig, ax) = plt.subplots(figsize=(10, 3))
+    ax.bar(
+        [i[0] for i in results], 
+        [i[1] for i in results],
+        width=delta, color='#502EBAAA'
     )
-    ratio = (wonCount+lostCount) and wonCount / (wonCount+lostCount) or 0
-    matchFreq = (wonCount+lostCount) / totalMatches
-    results.append((threshold, ratio, matchFreq))
-top = min([i[0] for i in results if i[1]==0])
-
-(fig, ax) = plt.subplots(figsize=(10, 3))
-ax.bar(
-    [i[0] for i in results], 
-    [i[1] for i in results],
-    width=delta, color='#502EBAAA'
-)
-for (x, v, f) in results:
-    if (v>0) and (x>0):
-        ax.hlines([f, ], x-delta/2, x+delta/2, color='#CB085688', lw=2)
-ax.bar(
-    np.arange(top, xlim+delta, delta), 
-    [1]*len(np.arange(top, xlim+delta, delta)),
-    width=delta, color='#7F7F9955'
-)
-ax.vlines(
-    np.arange(-delta/2, xlim+delta/2, 1), 
-    ymin=0, ymax=1, zorder=10, 
-    color='#000000CC', lw=1
-)
-ax.set_xticks(np.arange(0+delta/2, xlim+delta/2, 1))
-ax.set_xticklabels([int(i) for i in np.arange(0, xlim, 1)])
-ax.set_xlim(-delta/2, xlim+delta/2)
-ax.set_ylim(0, 1)
-ax.set_xlabel(stat)
-ax.set_ylabel('win ratio')
-ax.grid(False)
-fig.savefig(
-    path.join(oPath, f'{fNameID}_RateKAD.png'), 
-    dpi=500, bbox_inches='tight', facecolor=fig.get_facecolor()
-)
+    for (x, v, f) in results:
+        if (v>0) and (f<1):
+            ax.hlines([f, ], x-delta/2, x+delta/2, color='#CB0856DD', lw=2)
+    ax.bar(
+        np.arange(top, xlim+delta, delta), 
+        [1]*len(np.arange(top, xlim+delta, delta)),
+        width=delta, color='#7F7F9955'
+    )
+    ax.vlines(
+        np.arange(-delta/2, xlim+delta/2, 1), 
+        ymin=0, ymax=1, zorder=10, 
+        color='#000000CC', lw=1
+    )
+    ax.set_xticks(np.arange(0+delta/2, xlim+delta/2, 1))
+    ax.set_xticklabels([int(i) for i in np.arange(0, xlim, 1)])
+    ax.set_xlim(-delta/2, xlim+delta/2)
+    ax.set_ylim(0, 1)
+    ax.set_xlabel(stat)
+    ax.set_ylabel('win ratio')
+    ax.grid(False)
+    fig.savefig(
+        path.join(oPath, f'{fNameID}_RateKAD.png'), 
+        dpi=500, bbox_inches='tight', facecolor=fig.get_facecolor()
+    )
