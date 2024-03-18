809455ae033a746dc0d07e429b31edb044083e12
chipdelmal <chipdelmal@gmail.com>
2024-03-17 17:48:29 -0700

    Dataframe seems ready for use on bayesian

--- SplatStats/dev/devBayesian.py
+++ SplatStats/dev/devBayesian.py
@@ -3,24 +3,20 @@
 
 import numpy as np
 import pandas as pd
-from sys import argv
 from os import path
+import bambi as bmb
 from collections import Counter
 import SplatStats as splat
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_extraction import DictVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
 
-(PLYR, MATES, STATS) = (
+(PLYR, MATES, STATS, BATS) = (
     'čħîþ ウナギ',
-    ( 
-        'Yami ウナギ', 'Riché ウナギ', 'April ウナギ', 'Rei ウナギ',
+    [ 
+        'čħîþ ウナギ', 'Yami ウナギ', 'Riché ウナギ', 'April ウナギ', 'Rei ウナギ',
         'Oswal　ウナギ', 'Murazee'
-    ),
-    ('win', 'kill', 'death', 'assist', 'paint', 'special')
+    ],
+    ('win', 'kill', 'death', 'assist', 'paint', 'special'),
+    ('ko', 'matchType', 'duration', 'stage', 'festMatch')
 )
 (iPath, bPath, oPath) = (
     path.expanduser('~/Documents/BattlesDocker/jsons'),
@@ -44,15 +40,33 @@ bFilepaths = splat.getBattleFilepaths(bPath)
 plyr = splat.Player(PLYR, bFilepaths, timezone='America/Los_Angeles')
 playerHistory = plyr.battlesHistory
 phLen = playerHistory.shape[0]
+MATES.remove(PLYR)
 ###############################################################################
 # Examine battles
 ###############################################################################
-ix = 1
-(allies, enemies) = (
-    plyr.battleRecords[ix].alliedTeam,
-    plyr.battleRecords[ix].enemyTeams
-)
-# Get player row and outcome --------------------------------------------------
-plyrEntry = allies[allies['player name']==PLYR]
-plyrStats = {s: plyrEntry[s].values[0] for s in STATS}
-
+ix = 0
+nBattles = len(plyr.battleRecords)
+bList = []
+for ix in range(nBattles):
+    bRecord = plyr.battleRecords[ix]
+    (allies, enemies) = (bRecord.alliedTeam, bRecord.enemyTeams)
+    if PLYR not in set(allies['player name']):
+        continue
+    # Get battle stats --------------------------------------------------------
+    bttlStats = {s: bRecord.__getattribute__(s) for s in BATS}
+    # Get player row and outcome ----------------------------------------------
+    plyrEntry = allies[allies['player name']==PLYR]
+    plyrStats = {s: plyrEntry[s].values[0] for s in STATS}
+    # Get if allies are present -----------------------------------------------
+    allyPrsnt = {n: (n in set(allies['player name'])) for n in MATES}
+    # Assemble full dictionary ------------------------------------------------
+    bDicts = [bttlStats, plyrStats, allyPrsnt]
+    bList.append({k: v for d in bDicts for k, v in d.items()})
+# Generate dataframe and clean ------------------------------------------------
+df = pd.DataFrame.from_dict(bList)
+df = df[df['win']!='NA']
+df['win'] = df['win'].map({'W': 1, 'L': 0})
+###############################################################################
+# Return dataframe
+###############################################################################
+df
